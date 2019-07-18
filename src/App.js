68fb88e51c2b81226d72f5b9b72e2c959090cf62
Marques Johnson <marques.johnson1992@gmail.com>
2019-07-18 02:19:11 -0400

    here

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import Media from './components/Media/Media.js';
 
-function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
-  );
+const axios = require('axios');
+
+const nasaAPI = 'https://api.nasa.gov/planetary/apod?api_key=nfIXoKXfXb5pGxmbanV70k4dOopcTy9kW8U05nH8';
+
+ function App ({ limit }) {
+    //eslint-disable-next-line
+	const [	API, setAPI] = useState(nasaAPI);
+	const [	content, setContent] = useState('');
+
+	useEffect(() => {
+		axios.get(API)
+		.then(resp => {
+			setContent(resp); 
+			console.log(resp);
+		});		
+	 }, [API]);
+	 
+    console.log(content.url);
+    /*
+    
+    */
+   
+	return (
+		<div className="container">
+			<h1>Nasa Photo of the Day</h1> 
+			  
+                <Media imgUrl={content.url} />
+                <h2 className="title">{content.title}</h2>
+                <p className="date">{content.date}</p>
+                <p className="explanation">{content.explanation}</p>
+		     </div>
+	);
 }
 
 export default App;
---
+++ src/components/Media/Media.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Media (imgUrl, limit) {	
+	return (
+		<div className="iframeContainer">
+
+			<iframe src={imgUrl} title="video" width={'600'} height={'400'} />
+		</div>
+	);
+}
